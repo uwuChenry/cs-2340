f6bcbfaf2285c7998cd45f469d41d85e68b417c3
Chenry <henrychen0214@gmail.com>
2026-09-09 15:00:53 -0400

    boostrap  not work

--- moviesstore/home/views.py
+++ moviesstore/home/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    template_data = {}
+    template_data['title'] = 'Movies Store'
+    return render(request, 'home/index.html', {'template_data': template_data}) 
+
+def about(request):
+    return render(request, 'home/about.html')
+
