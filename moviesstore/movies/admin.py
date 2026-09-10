2201476ddd28ff0edb1cb18bb218e120aaf6cfdf
Chenry <henrychen0214@gmail.com>
2026-09-09 22:35:57 -0400

    finished mysql

--- moviesstore/movies/admin.py
+++ moviesstore/movies/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Movie
+
+class MovieAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    search_fields = ['name']
+
+
 admin.site.register(Movie)
 
